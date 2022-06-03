296466cbe65e52d5edad9bff8e49d17b2bb59a40
Hannes Hertach <hanneshertach@gmail.com>
2022-06-03 11:07:48 +0200

    Add question to add core components

--- generators/add-franklin/index.js
+++ generators/add-franklin/index.js
@@ -1,6 +1,7 @@
 const Generator = require('yeoman-generator')
 const path = require('path')
 const { addDependencies, addPkgScript, writeKeyAppConfig } = require('../../lib/utils')
+const { isLoopingPrompts } = require('../../lib/constants')
 
 class FranklinGenerator extends Generator {
   constructor (args, opts) {
@@ -11,15 +12,51 @@ class FranklinGenerator extends Generator {
   async prompting () {
     // Default tenant if we skip all prompts
     this.tenant = 'demo-site'
+    this.coreComponents = []
     if (!this.options['skip-prompt']) {
       const res = await this.prompt([
         {
           type: 'input',
           name: 'tenant',
-          message: 'What is the name of the AEM tennant you would like to use?'
+          message: 'What is the name of the AEM tenant you would like to use?',
+          loop: isLoopingPrompts
+        },
+        {
+          type: 'checkbox',
+          name: 'coreComponents',
+          message: 'Which AEM Core Components should we generate stubs to customise for?',
+          loop: isLoopingPrompts,
+          choices: [
+            {
+              name: 'Teaser',
+              value: 'teaser',
+              checked: false
+            },
+            {
+              name: 'Title',
+              value: 'title',
+              checked: false
+            },
+            {
+              name: 'Text',
+              value: 'text',
+              checked: false
+            },
+            {
+              name: 'Container',
+              value: 'container',
+              checked: false
+            },
+            {
+              name: 'Separator',
+              value: 'separator',
+              checked: false
+            }
+          ]
         }
       ])
       this.tenant = res.tenant
+      this.coreComponents = res.coreComponents
     }
   }
 
@@ -29,12 +66,21 @@ class FranklinGenerator extends Generator {
     this.fs.copyTpl(
       `${this.templatePath()}/**`,
       this.destinationPath(),
-      this.props)
+      this.props,
+      undefined, { globOptions: { ignore: ['**/template-core-component/**'] } })
 
     addDependencies(this, { 'franklin-esr': 'adobe-rnd/franklin-esr' })
     addDependencies(this, { 'patch-package': '^6.4.7' }, true)
     addPkgScript(this, { build: 'franklin build', dev: 'franklin dev', postinstall: 'patch-package' })
     writeKeyAppConfig(this, 'franklin.tenant', this.tenant)
+
+    this.coreComponents.forEach((component) => {
+      this.fs.copyTpl(
+        this.templatePath('components/template-core-component/index.jsx'),
+        this.destinationPath(`components/${component}/index.jsx`),
+        { componentName: component }
+      )
+    })
   }
 }
 
---
+++ generators/add-franklin/templates/components/template-core-component/index.jsx
@@ -0,0 +1,3 @@
+export default function Teaser() {
+  return <p><%=componentName%>: Not Implemented</p>
+}
